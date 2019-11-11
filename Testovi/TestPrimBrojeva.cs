8ad25e943b6c9db18fac3bab8a6adaad25da33e6
ivana042 <ivzavrski@vsite.hr>
2019-11-11 20:24:47 +0100

    Implement Unit Tests

--- Testovi/TestPrimBrojeva.cs
+++ Testovi/TestPrimBrojeva.cs
@@ -7,8 +7,29 @@ namespace Vsite.Pood
     public class TestPrimBrojeva
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GenerirajPrimBrojeveVraćaPrazanNizZaArgument0()
         {
+            Assert.AreEqual(0, PrimBrojevi.GenerirajPrimBrojeve(0).Length);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveVraćaPrazanNizZaArgument1()
+        {
+            Assert.AreEqual(0, PrimBrojevi.GenerirajPrimBrojeve(1).Length);
+        }
+        [TestMethod]
+        public void GenerirajPrimBrojeveVraćaNizKojiSadržiSamo2ZaArgument2()
+        {
+            Assert.AreEqual(1, PrimBrojevi.GenerirajPrimBrojeve(2).Length);
+            Assert.AreEqual(2, PrimBrojevi.GenerirajPrimBrojeve(2)[0]);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveVraćaNizKojiSadrži25ProstihBrojevaZaArgument100()
+        {
+            Assert.AreEqual(25, PrimBrojevi.GenerirajPrimBrojeve(100).Length);
+            Assert.AreEqual(2, PrimBrojevi.GenerirajPrimBrojeve(100)[0]);
+            Assert.AreEqual(97, PrimBrojevi.GenerirajPrimBrojeve(100)[24]);
         }
     }
 }
