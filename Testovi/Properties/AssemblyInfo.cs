98266c93401073dfc33e2b95749b4752189c547c
ivana042 <ivzavrski@vsite.hr>
2019-11-11 20:09:46 +0100

    Add test project

---
+++ Testovi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testovi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testovi")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f1f8545-13be-442a-ac17-35f2baca0c7a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testovi/TestPrimBrojeva.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Pood
+{
+    [TestClass]
+    public class TestPrimBrojeva
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
